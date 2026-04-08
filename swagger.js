67df7e6fa750283f6f2a84166a0e653725c22ab7
Rahul singh <rahuls6408@gmail.com>
2026-04-08 12:25:16 +0530

    Updated swagger for authorization

--- swagger.js
+++ swagger.js
@@ -1,15 +1,38 @@
-import swaggerJSDoc from "swagger-jsdoc";
-const options = {
+import swaggerJsdoc from 'swagger-jsdoc';
+
+const swaggerOptions = {
   definition: {
     openapi: '3.0.0',
     info: {
       title: 'Employee API',
       version: '1.0.0',
+      description: 'Employee Management API',
     },
+    servers: [
+      {
+        url: 'http://localhost:3000',
+        description: 'Development server',
+      },
+    ],
+    components: {
+      securitySchemes: {
+        bearerAuth: {
+          type: 'http',
+          scheme: 'bearer',
+          bearerFormat: 'JWT',
+          description: 'Enter your JWT token',
+        },
+      },
+    },
+    security: [
+      {
+        bearerAuth: [],
+      },
+    ],
   },
   apis: ['./routes/*.js'],
 };
 
-const swaggerSpec = swaggerJSDoc(options);
+const swaggerSpec = swaggerJsdoc(swaggerOptions);
 
-export default swaggerSpec
+export default swaggerSpec;
