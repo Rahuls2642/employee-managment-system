45ea84c06742f00da4dbf69499c9902ff7f92b2b
Rahul singh <rahuls6408@gmail.com>
2026-04-09 10:00:16 +0530

    fixed some bugs

--- controller/attendance.controller.js
+++ controller/attendance.controller.js
@@ -1,46 +1,56 @@
-import db from "../db.js"
+import db from "../db.js";
 
-export const attendaceSummary=async(req,res)=>{
-    const {employee_id}=req.query
-let query=` SELECT e.employee_name,a.employee_id,
-SUM(CASE WHEN is_full_day THEN 1 ELSE 0.5 END) as total_days
-FROM attendance a
-JOIN employee e ON a.employee_id=e.employee_id
-GROUP BY a.employee_id, e.employee_name`
+//Attendance summarey logic
+export const attendaceSummary = async (req, res) => {
+  const { employee_id, month } = req.query;
+  
+  let query = `
+    SELECT e.employee_name, a.employee_id,
+    SUM(CASE WHEN is_full_day THEN 1 ELSE 0.5 END) as total_days
+    FROM attendance a
+    JOIN employee e ON a.employee_id = e.employee_id
+    WHERE 1=1
+  `;
 
-const value=[]
-if(employee_id){
-  value.push(employee_id);
-  query += ` HAVING a.employee_id = $${value.length}`
+  const value = [];
 
-}
-const result=await db.query(query,value);
+  if (employee_id) {
+    value.push(parseInt(employee_id));
+    query += ` AND a.employee_id = $${value.length}`;
+  }
 
+  if (month) {
+    value.push(parseInt(month));
+    query += ` AND EXTRACT(MONTH FROM a.attendance_date) = $${value.length}`;
+  }
 
-    res.json(result.rows);
-}
+  query += ` GROUP BY a.employee_id, e.employee_name`;
 
-export const attendace=async(req,res)=>{
-     const {attendance_date,employee_id,is_full_day}=req.body;
-    const result=await db.query(
-        `INSERT INTO attendance
+  const result = await db.query(query, value);
+  res.json(result.rows);
+};
+
+export const attendace = async (req, res) => {
+  const { attendance_date, employee_id, is_full_day } = req.body;
+  const result = await db.query(
+    `INSERT INTO attendance
         (attendance_date,employee_id,is_full_day)
         VALUES($1,$2,$3) RETURNING *`,
-        [attendance_date, employee_id, is_full_day]
-    )
-    res.json(result.rows[0]);
-}
+    [attendance_date, employee_id, is_full_day],
+  );
+  res.json(result.rows[0]);
+};
 
-export const attendaceList=async(req,res)=>{
-     const {employee_id,date}=req.query;
-    let query=`
+export const attendaceList = async (req, res) => {
+  const { employee_id, date } = req.query;
+  let query = `
     SELECT e.employee_name,a.attendance_date,a.is_full_day
     FROM attendance a
     JOIN employee e ON a.employee_id = e.employee_id
     WHERE 1=1
     `;
-    const values=[]
-    if (employee_id) {
+  const values = [];
+  if (employee_id) {
     values.push(employee_id);
     query += ` AND a.employee_id = $${values.length}`;
   }
@@ -48,6 +58,6 @@ export const attendaceList=async(req,res)=>{
     values.push(date);
     query += ` AND a.attendance_date = $${values.length}`;
   }
-    const result = await db.query(query, values);
+  const result = await db.query(query, values);
   res.json(result.rows);
-}
+};
--- controller/auth.controller.js
+++ controller/auth.controller.js
@@ -1,72 +1,73 @@
-import db from "../db.js"
-import bcrypt from "bcrypt"
-import jwt from "jsonwebtoken"
+import db from "../db.js";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
-const SECRET_KEY=process.env.SECRET_KEY;
-//Register user
-export const registerUser=async(req,res)=>{
-    if (!SECRET_KEY) {
+const SECRET_KEY = process.env.SECRET_KEY;
+
+//Regstr user
+export const registerUser = async (req, res) => {
+  if (!SECRET_KEY) {
     console.error("ERROR: SECRET_KEY not set in .env file");
-}
+  }
 
-   try {
-    const{name,mobile_no,password}=req.body;
-    //check if user already exist
-  
-    const existing=await db.query(
-        ` SELECT * FROM users WHERE mobile_no=$1`,
-        [mobile_no]
-    );
-    if(existing.rows.length>0){
-        return res.status(400).json({error:"Mobile no already registeres use diffrent mobile number"})
+  try {
+    const { name, mobile_no, password } = req.body;
+    if(!name,!mobile_no,!password){
+        return res.status(400).res.json({error:"Plesse enter all the fields"})
     }
-    //hash password
-    const hashedPassword=await bcrypt.hash(password,10)
 
-    const result=await db.query(
-        ` INSERT INTO users (name, mobile_no, password)
+    const existing = await db.query(` SELECT * FROM users WHERE mobile_no=$1`, [
+      mobile_no,
+    ]);
+    if (existing.rows.length > 0) {
+      return res
+        .status(400)
+        .json({
+          error: "Mobile no already registeres use diffrent mobile number",
+        });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const result = await db.query(
+      ` INSERT INTO users (name, mobile_no, password)
         VALUES($1, $2, $3) RETURNING user_id, name, mobile_no`,
-        [name, mobile_no, hashedPassword]
+      [name, mobile_no, hashedPassword],
     );
-    res.json({message:"Registered succesfully", user:result.rows[0]})
-    
-   } catch (error) {
+    res.json({ message: "Registered succesfully", user: result.rows[0] });
+  } catch (error) {
     res.status(500).json({ error: error.message });
-    
-   } 
-}
+  }
+};
 
 //Login user
-export const loginUser=async(req,res)=>{
-    try {
-        const {mobile_no,password}=req.body;
-        //check if mobile no exist
-        const result=await db.query(
-            ` SELECT * FROM users WHERE mobile_no=$1`,
-            [mobile_no]
-        )
-        if(result.rows.length===0){
-            return res.status(400).json({error:"Invalid details"});
-        }
-        const user=result.rows[0];
+export const loginUser = async (req, res) => {
+  try {
+    const { mobile_no, password } = req.body;
+    //check if mobile no exist
+    const result = await db.query(` SELECT * FROM users WHERE mobile_no=$1`, [
+      mobile_no,
+    ]);
+    if (result.rows.length === 0) {
+      return res.status(400).json({ error: "Invalid details" });
+    }
+    const user = result.rows[0];
 
     //check password
-    const isMatch=await bcrypt.compare(password,user.password);
-    if(!isMatch){
-        return res.status(400).json({error:"Invalid details"});
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(400).json({ error: "Invalid details" });
     }
 
-    //token 
+    //token
 
-    const token=jwt.sign(
-        {user_id:user.user_id, name:user.name},
-        SECRET_KEY,
-        {expiresIn:'1d'}
+    const token = jwt.sign(
+      { user_id: user.user_id, name: user.name },
+      SECRET_KEY,
+      { expiresIn: "1d" },
     );
-    res.json({message:"Login succesfull",token});
-
-
-    } catch (error) {
-        res.status(500).json({ error: error.message });
-    }
-}
+    res.json({ message: "Login succesfull", token });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
